69ce27204ab510d4a2d71ab3d6095ba58f9bd665
HarrisonBirch <csmhbirc@ljmu.ac.uk>
2026-02-27 16:14:44 +0000

    Update Surgery.java

--- OOsysCSW/src/cswk/Surgery.java
+++ OOsysCSW/src/cswk/Surgery.java
@@ -32,24 +32,19 @@ public class Surgery {
 
 	}
 	public void cancelBooking() {
-		//ask for uuid and find uuid, 
-		//then give customer opportunity to remove object in array
+
 
 	}
 	public void searchBooking() {
-		//somehow make a way to create future booking slots in an array, 
-		//this could be the purpose of booking class
+
 
 	}
-	public void Register() {
-		//the staff can search up booking that are already booked or new booking, and can assign themselves
-		//nurse definitely and surgeon might have to be consulting
-		//receptionist can do the same but with patients has to be new
-	}
+
 	public void Searching() {
-		//staff can search their own bookings, receptionist can search patients bookings
+
 	}
 	public void checkAvailability() {
-		//method to see what bookings are available
+
 	}
 }
+
